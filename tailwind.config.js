62040c80a83537cd1a83edcbc19201eb17d08c73
Ryzen_10 <mr.x000001111122222@gmail.com>
2024-01-20 19:06:16 +0530

    Added RazorPay Project

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+    fontFamily: {
+      mullish: ["Mulish", "sans-serif"],
+    },
+    colors: {
+      deepBlue: "#02042a",
+      lightBlue: "#2b84ea",
+      lightBluegrad:"#2f70f5",
+      lightBlue300: "#4b94ed",
+      lightBlue500: "#0b72e7",
+      greenLight: "#61cea6",
+      grayText: "#818597",
+      lightGray: "#e2e2e2",
+      grayBlue: "#344a6c",
+      deepBlueHead: "#162f56",
+      gray2: "#525a76",
+      blue_grad:"#e4effd",
+      blue_bor:"#cce2fc",
+      deepBlue2:"#181c2e",
+      gray_animation:"#f5f8fe",
+    },},
+  },
+  plugins: [],
+}
